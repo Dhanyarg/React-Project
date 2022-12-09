edb24add409369c7da23cf2b256a4d6ff4077301
Dhanyarg <dhanya@seemymachines.com>
2022-12-09 16:00:50 +0530

    Heading & Paragragh

---
+++ first-app/second-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello EveryOne....Listen to the Latest FIFA News
+        <br />
+        <br />
+        <p>
+          FIFA is working on a wide range of topics from improving the women's
+          game and developing football <br />
+          around the world to ensuring better football governance structures.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
